553255b03f455401f8acdd481ef0de21b6a926ed
Daniel <daniru9@gmail.com>
2020-09-20 19:48:06 +0200

    boton de compartir funcional, instalado otro plugin para ello

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
+import { ActionSheetController } from '@ionic/angular';
+
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
 
 @Component({
   selector: 'app-noticia',
@@ -13,12 +17,47 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() i: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser,
+              private actionSheetController: ActionSheetController,
+              private socialSharing: SocialSharing) { }
 
   ngOnInit() {}
 
   abrirNoticia(){
     const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu(){
+    const actionSheet = await this.actionSheetController.create({
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share-social',
+        handler: () => {
+          console.log('Share clicked');
+          this.socialSharing.share(
+            this.noticia.title,
+            this.noticia.source.name,
+            '',
+            this.noticia.url
+          );
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'star',
+        handler: () => {
+          console.log('Favorito');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
