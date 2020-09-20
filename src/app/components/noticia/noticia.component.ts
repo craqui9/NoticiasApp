9ff6693fe063d44bc5b8f557e37a39288234effd
Daniel <daniru9@gmail.com>
2020-09-20 16:57:51 +0200

    Abrir las noticias al hacer click, instalado el plugin inappbrowser para ello

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,8 +13,12 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() i: number;
 
-  constructor() { }
+  constructor(private iab: InAppBrowser) { }
 
   ngOnInit() {}
+
+  abrirNoticia(){
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
 
 }
